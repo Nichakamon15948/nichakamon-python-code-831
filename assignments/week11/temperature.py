4de2b828621171510d1e4e58696f6ee177c3c008
Nichakamon Tapaohirun <nichakamon.ta@ku.th>
2025-10-07 14:52:24 +0700

    week11

--- assignments/week11/temperature.py
+++ assignments/week11/temperature.py
@@ -8,7 +8,7 @@ Create a function get_temperatures() that:
 
 Create a function analyze_temps(temp_list) that:
 
-- Calculates and returns the average temperature (local variable)
+- Calculates and returns the average temperature (local variable) 
 - Finds and returns the highest temperature
 - Finds and returns the lowest temperature
 - Returns all three values as a tuple
@@ -22,4 +22,29 @@ Highest: 28 C
 Lowest: 19 C
 
 """
+
+def get_temperatures():
+    temperatures = [31, 32, 30, 29, 28, 31, 32]
+    return temperatures
+
+def analyze_temps(temp_list):
+    avg_temp = 0
+    highest_temp = max(temp_list)
+    lowest_temp = min(temp_list)
+
+    sum = 0
+    for temp in temp_list:
+        sum = sum + temp
+        avg_temp = sum / len(temp_list)
     
+    return(avg_temp, highest_temp, lowest_temp)
+    
+def display_analysis(avg, high, low):
+    print("Temperature Analysis for the Week:")
+    print("Average: %.2f C" %(avg))
+    print(f"Highest: {high} C")
+    print("Lowest: ", low, " C")
+
+my_temps = get_temperatures()
+analyzed_temps = analyze_temps(my_temps)
+display_analysis(analyzed_temps[0], analyzed_temps[1], analyzed_temps[2])
